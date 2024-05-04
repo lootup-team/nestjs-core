9bdb70b198dd9efb17af28c632c21a7cf96a68b8
gm50x <getuliomagela@outlook.com>
2024-05-04 18:34:46 -0300

    removes config module

--- src/context/context.module.ts
+++ src/context/context.module.ts
@@ -1,12 +1,10 @@
 import { Global, Module, NestModule } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 import { ContextManager } from './context.manager';
 import { ConfigurableModuleClass } from './context.module-builder';
 import { ContextService } from './context.service';
 
 @Global()
 @Module({
-  imports: [ConfigModule.forRoot()],
   providers: [ContextManager, ContextService],
   exports: [ContextService],
 })
